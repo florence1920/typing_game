2452b4a289fc1af4a659c82802adf13497e195e2
lsh <florence1920>
2023-07-07 15:36:48 +0900

    com

--- index.js
+++ index.js
@@ -7,128 +7,129 @@ const before = document.querySelector('.before');
 const start = document.querySelector('.start');
 const end = document.querySelector('.end');
 //start 버튼 눌렀을 때 
-startBtn.addEventListener('click', function(){
+startBtn.addEventListener('click', function () {
     //화면 조정
     before.style.display = 'none';
     start.style.display = 'block';
 
-    stageStart();
+    checkKey();
 })
 
-//stage
-let stage = 1;
 
-
-
-function stageStart(){
-    //키보드 배열 생성
-    const origin = ['q','w','e','a','s','d'];
-    const key = document.querySelector('.key');
-    let keySet = [];
+let keySet = [];
+//키보드 배열 생성
+function makeKeySet() {
+    console.log('makeSet');
+    keySet = [];
+    const origin = ['q', 'w', 'e', 'a', 's', 'd'];
+    let key = document.querySelector('.key');
+    key.replaceChildren();
     //배열 화면에 나오게
-    for (let i = 0; i < 6; i++){
+    for (let i = 0; i < 6; i++) {
         let li = document.createElement("li");
         let random = Math.floor(Math.random() * 6);
         keySet.push(origin[random]);
         li.append(origin[random]);
         key.append(li)
     }
-    console.log(keySet)
-    let step = 1;
-    console.log(`000${step}`)
-    // 키 확인
-    function checkKey (){
-        countStart();
-        let keyCount = 0;
-        window.addEventListener("keyup", (e) => {
-            console.log(e.key)
-            console.log(`keyCount : ${keyCount}`)
-            console.log(`keySet: ${keySet}`)
-            console.log(keySet[keyCount])
-            console.log(`step: ${step}`)
-            
-            if(e.key === keySet[keyCount]){
-                getClass(step);
-                step = step + 1;
-                if(step === 7 && timeSafe){
-                    console.log('지금!!')
-                    document.querySelector('.clear').innerHTML='Clear'
-                    stage = stage+1;
-                    if(stage <=2){
-                        key.replaceChildren();
-                        stageStart();
-                    }
-                }
-            }else{
-                fail();
-            }
-            //시간초과
-            if(timeSafe === false){
-                fail();
-            }
-           
-            keyCount = keyCount + 1;
-
-            
-            
-            console.log('-------------')
-        });
-        
-    }
-    const progressBar = document.getElementById("bar");
-    let timeSafe = true;
-
+}
+let step = 1;
+// 키 확인
+function checkKey() {
     //카운터 시작
-    function countStart(){
+    function countStart() {
         //시간 진행되게
         let barWidth = 0;
 
         const animate = () => {
-            barWidth = barWidth + 2.5;
+            barWidth = barWidth + 1.25;
             progressBar.style.width = `${barWidth}%`;
         };
 
         // animation starts 2 seconds after page load
         setTimeout(() => {
             let intervalID = setInterval(() => {
-            if(step === 7){
-                clearInterval(intervalID);
-            }
-            if (barWidth >= 100) {
-                timeSafe = false;
-                clearInterval(intervalID);
-                if(step !== 7){
-                    fail();
+                console.log(step);
+                if (step === 7) {
+                    console.log('step 7!!')
+                    clearInterval(intervalID);
                 }
-            } else {
-                animate();
-            }
+                if (barWidth >= 100) {
+                    timeSafe = false;
+                    clearInterval(intervalID);
+                    if (step !== 7) {
+                        final(false);
+                    }
+                } else {
+                    animate();
+                }
             }, 100); //this sets the speed of the animation
-        }, );
-        //진행바 넘어가게
-
+        },);
     }
-    checkKey();
+
+    makeKeySet();
+    console.log('checkKey');
+    countStart();
+    let keyCount = 0;
+    console.log(keySet)
+    console.log(keyCount)
+    let stage = 1;
+    window.addEventListener("keypress", (e) => {
+        console.log(`눌러야하는 키 : ${keySet[keyCount]}`)
+        console.log(`누른 키 : ${e.key}`)
+        if (e.key === keySet[keyCount]) {
+            getClass(step);
+            step = step + 1;
+            keyCount = keyCount + 1;
+            console.log(`step:${step}`)
+            if (step === 7) {
+
+                stage = stage + 1;
+                console.log(stage)
+                if (stage === 4) {
+                    final(true);
+                }
+                makeKeySet();
+                console.log(keySet)
+
+                keyCount = 0;
+                step = 1;
+            }
+        } else {
+            final(false);
+        }
+
+        console.log('------------')
+    });
 }
 
+const progressBar = document.getElementById("bar");
+let timeSafe = true;
+
+
+
+
 //클래스 주는 함수 
-function getClass(num){
+function getClass(num) {
     let now = document.querySelector('.now');
-    if (now !== null){
+    if (now !== null) {
         now.classList.remove('now');
     }
     let need = document.querySelector(`.key li:nth-child(${num})`);
-    need.className='now';
+    console.log(need);
+    console.log(need);
+    need.className = 'now';
 }
 
 //실패 시!
-function fail(){
+function final(win) {
     start.style.display = 'none';
     end.style.display = 'block';
+    let result = document.querySelector('.result');
+    if (win) {
+        result.innerText = '성공!!'
+    } else {
+        result.innerText = '땡!'
+    }
 }
 
-console.log(`stage : ${stage}`)
-
-if(stage === 2) {
-    stageStart();
-}
